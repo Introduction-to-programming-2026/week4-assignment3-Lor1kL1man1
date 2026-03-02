b5e9a167f93c4e14bb538da4c7ad0ee245b67606
Lor1kL1man1 <limani.lorik@ibu.edu.mk>
2026-03-02 22:28:43 +0000

    Mon, Mar 2, 2026, 11:28 PM +01:00

--- src/iteration.c
+++ src/iteration.c
@@ -25,29 +25,36 @@ Rules:
 */
 // Draws a pyramid using iteration
 
-#include <cs50.h>
-#include <stdio.h>
+#include <cs50.h>   // Library for get_int()
+#include <stdio.h>  // Library for printf()
 
+// Function prototype: declares a function that draws the pyramid
 void draw(int n);
 
 int main(void)
 {
-    // Get height of pyramid
+    // Ask the user to enter the height of the pyramid
     int height = get_int("Height: ");
 
-    // Draw pyramid
+    // Call the draw function and pass the height
     draw(height);
 }
 
+// Function that prints a left-aligned pyramid of height n
 void draw(int n)
 {
-    // Draw pyramid of height n
+    // Outer loop controls the number of rows
+    // It runs n times (from 0 to n-1)
     for (int i = 0; i < n; i++)
     {
+        // Inner loop prints the correct number of # symbols per row
+        // For row i, it prints (i + 1) hashes
         for (int j = 0; j < i + 1; j++)
         {
             printf("#");
         }
+
+        // After printing hashes, move to the next line
         printf("\n");
     }
 }

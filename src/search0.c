bf56cfa26b614b635b2c36be4352457a619b380c
Lor1kL1man1 <limani.lorik@ibu.edu.mk>
2026-03-03 11:24:19 +0100

    finished fro the second time

--- src/search0.c
+++ src/search0.c
@@ -17,3 +17,27 @@ Rules:
 - You must use a loop.
 - Do NOT use any built-in search function.
 */
+
+
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main(void){
+
+   int numbers[]= {20,500,10,5,100,1,50};
+
+   int n = get_int("What number are you searching for? ");
+
+   for(int i = 0; i < 7; i ++){
+
+      if (numbers[i] == n){
+         printf("Found\n");
+      }
+   }
+      printf("Not Found\n");
+      return 1;
+
+
+
+}
